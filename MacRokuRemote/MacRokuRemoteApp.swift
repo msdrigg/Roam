9f292676a5ea87c7f02ccc7d1623faf3cab2974f
Scott Driggers <github@msd3.io>
2023-10-08 20:57:31 -0400

    Initial commit

--- MacRokuRemote/MacRokuRemoteApp.swift
+++ MacRokuRemote/MacRokuRemoteApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct MacRokuRemoteApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+            Device.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
@@ -25,7 +25,7 @@ struct MacRokuRemoteApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RokuRemoteView()
         }
         .modelContainer(sharedModelContainer)
     }
