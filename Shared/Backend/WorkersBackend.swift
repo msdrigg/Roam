e7c0cfb2e26bcdc0308c2b5a21d07c732f587395
Scott Driggers <github@msd3.io>
2025-05-04 20:22:36 -0400

    Fixing backend issue

--- Shared/Backend/WorkersBackend.swift
+++ Shared/Backend/WorkersBackend.swift
@@ -5,8 +5,8 @@ import OSLog
 import SwiftData
 import UniformTypeIdentifiers
 
-// private let globalBackendURL = "https://backend.roam.msd3.io"
-private let globalBackendURL = "http://localhost:8080"
+ private let globalBackendURL = "https://backend.roam.msd3.io"
+//private let globalBackendURL = "http://localhost:8080"
 
 private func getAPIKey() -> String? {
     let apiKey = Bundle.main.infoDictionary?["BACKEND_API_KEY"] as? String
