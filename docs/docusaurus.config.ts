6cb8c4c9a67ec13aeffe668f3362d7682ce8ad57
Scott Driggers <github@msd3.io>
2026-06-01 16:36:41 -0400

    WIP2

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -1,84 +1,95 @@
-import { themes as prismThemes } from 'prism-react-renderer';
-import type { Config } from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: 'Roam for Roku',
-  tagline: 'A you-first remote for Roku',
-  favicon: 'img/favicon.ico',
+    title: "Roam for Roku",
+    tagline: "A you-first remote for Roku",
+    favicon: "img/favicon.ico",
 
-  url: 'https://roam.msd3.io',
-  baseUrl: '/',
+    url: "https://roam.msd3.io",
+    baseUrl: "/",
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+    onBrokenLinks: "throw",
+    markdown: { hooks: { onBrokenMarkdownLinks: "warn" } },
 
-  i18n: {
-    defaultLocale: 'en',
-    locales: [
-      "en", "ar", "de", "es", "es-419",
-      "fil", "fr", "fr-CA", "it", "pa",
-      "pt-BR", "pt-PT", "vi", "zh-Hans"
+    i18n: {
+        defaultLocale: "en",
+        locales: [
+            "en",
+            "ar",
+            "de",
+            "es",
+            "es-419",
+            "fil",
+            "fr",
+            "fr-CA",
+            "it",
+            "pa",
+            "pt-BR",
+            "pt-PT",
+            "vi",
+            "zh-Hans",
+        ],
+    },
+
+    presets: [
+        [
+            "classic",
+            {
+                docs: false,
+                blog: false,
+
+                theme: {
+                    customCss: "./src/css/custom.css",
+                },
+            } satisfies Preset.Options,
+        ],
     ],
-  },
 
-  presets: [
-    [
-      'classic',
-      {
-        docs: false,
-        blog: false,
-
-        theme: {
-          customCss: './src/css/custom.css',
+    themeConfig: {
+        // Replace with your project's social card
+        image: "img/docusaurus-social-card.jpg",
+        navbar: {
+            title: "Roam",
+            logo: {
+                alt: "Roam App Icon",
+                src: "img/roam-icon.png",
+                style: {
+                    "border-radius": "20%",
+                },
+            },
+            items: [
+                {
+                    href: "https://github.com/msdrigg/roam",
+                    label: "GitHub",
+                    position: "right",
+                },
+                {
+                    type: "localeDropdown",
+                    position: "right",
+                },
+            ],
         },
-      } satisfies Preset.Options,
-    ],
-  ],
-
-  themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
-    navbar: {
-      title: 'Roam',
-      logo: {
-        alt: 'Roam App Icon',
-        src: 'img/roam-icon.png',
-        style: {
-          "border-radius": "20%"
-        }
-      },
-      items: [
-        {
-          href: 'https://github.com/msdrigg/roam',
-          label: 'GitHub',
-          position: 'right',
+        footer: {
+            style: "dark",
+            links: [
+                {
+                    label: "Github",
+                    href: "https://github.com/msdrigg/roam",
+                },
+                {
+                    label: "Discord",
+                    href: "https://discord.gg/FqaTNRccbG",
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} Scott Driggers`,
         },
-        {
-          type: 'localeDropdown',
-          position: 'right',
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
         },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          label: 'Github',
-          href: 'https://github.com/msdrigg/roam'
-        },
-        {
-          label: 'Discord',
-          href: 'https://discord.gg/FqaTNRccbG',
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} Scott Driggers`,
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-    },
-  } satisfies Preset.ThemeConfig,
+    } satisfies Preset.ThemeConfig,
 };
 
 export default config;
